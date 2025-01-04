00dba42fa26ee7b17afbdea9334417b91e5558f8
Phyo-Aung-Naing-Tun <phyotun24@gmail.com>
2025-01-04 17:20:00 +0630

    added 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "../views/HomeView.vue";
+import { h } from "vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,6 +20,11 @@ const router = createRouter({
       path: "/project/:id",
       name: "single-project",
       component: () => import("@/views/ProjectOneView.vue"),
+    },
+    {
+      path: "/:catchAll(.*)*",
+      name: "NotFound",
+      component: h("p", { style: "color: red" }, "404 Not Found"),
     },
   ],
 });
