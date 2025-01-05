34d34eb3a2fb077cdd160ecf5a801400d1d0f106
Phyo-Aung-Naing-Tun <phyotun24@gmail.com>
2025-01-05 22:49:17 +0630

    added table

--- src/lib/superBaseClient.ts
+++ src/lib/superBaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://vnjpojzoeuzayldaoemc.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZuanBvanpvZXV6YXlsZGFvZW1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU5OTM3MTgsImV4cCI6MjA1MTU2OTcxOH0.o6322o8K7po_YD7oD7oeGhAKCbFNg0h98NjuMYZLb9o"
+  import.meta.env.VITE_SUPERBASE_URL,
+  import.meta.env.VITE_SUPERBASE_KEY
 );
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,3 +4,20 @@
     <router-link to="/project">Go To Project</router-link>
   </div>
 </template>
+<script setup>
+import { ref, onMounted } from "vue";
+import { supabase } from "@/lib/superBaseClient";
+
+const countries = ref([]);
+
+async function getCountries() {
+  const { data } = await supabase.from("projects").select();
+  console.log(data);
+
+  countries.value = data;
+}
+
+onMounted(() => {
+  getCountries();
+});
+</script>
